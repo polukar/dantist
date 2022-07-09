558e3d442040aa340243ece5fee0801d48f7ea1e
polukar <polukarov.qweewq@gmail.com>
2022-07-09 16:07:40 +0300

    bug fix

--- js/main.js
+++ js/main.js
@@ -66,12 +66,56 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _project_video__WEBPACK_IMPORTED_MODULE_4___default = /*#__PURE__*/__webpack_require__.n(_project_video__WEBPACK_IMPORTED_MODULE_4__);
 /* harmony import */ var _project_map__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ./project/map */ "./src/js/project/map.js");
 /* harmony import */ var _project_map__WEBPACK_IMPORTED_MODULE_5___default = /*#__PURE__*/__webpack_require__.n(_project_map__WEBPACK_IMPORTED_MODULE_5__);
+/* harmony import */ var _project_anchor__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ./project/anchor */ "./src/js/project/anchor.js");
+/* harmony import */ var _project_anchor__WEBPACK_IMPORTED_MODULE_6___default = /*#__PURE__*/__webpack_require__.n(_project_anchor__WEBPACK_IMPORTED_MODULE_6__);
 
 
 
 
 
 
+
+
+/***/ }),
+
+/***/ "./src/js/project/anchor.js":
+/*!**********************************!*\
+  !*** ./src/js/project/anchor.js ***!
+  \**********************************/
+/***/ (function() {
+
+function _createForOfIteratorHelper(o, allowArrayLike) { var it = typeof Symbol !== "undefined" && o[Symbol.iterator] || o["@@iterator"]; if (!it) { if (Array.isArray(o) || (it = _unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") { if (it) o = it; var i = 0; var F = function F() {}; return { s: F, n: function n() { if (i >= o.length) return { done: true }; return { done: false, value: o[i++] }; }, e: function e(_e) { throw _e; }, f: F }; } throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); } var normalCompletion = true, didErr = false, err; return { s: function s() { it = it.call(o); }, n: function n() { var step = it.next(); normalCompletion = step.done; return step; }, e: function e(_e2) { didErr = true; err = _e2; }, f: function f() { try { if (!normalCompletion && it.return != null) it.return(); } finally { if (didErr) throw err; } } }; }
+
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
+var anchors = document.querySelectorAll('a[href*="#"]');
+
+var _iterator = _createForOfIteratorHelper(anchors),
+    _step;
+
+try {
+  var _loop = function _loop() {
+    var anchor = _step.value;
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      var blockID = anchor.getAttribute('href').substr(1);
+      document.getElementById(blockID).scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+      });
+    });
+  };
+
+  for (_iterator.s(); !(_step = _iterator.n()).done;) {
+    _loop();
+  }
+} catch (err) {
+  _iterator.e(err);
+} finally {
+  _iterator.f();
+}
 
 /***/ }),
 
@@ -92,6 +136,15 @@ if (header) {
   });
   close.addEventListener('click', function () {
     mibileMenu.classList.remove('active');
+  });
+  document.addEventListener('scroll', function () {
+    var posTop = document.body.getBoundingClientRect().top;
+
+    if (-posTop > 200) {
+      header.classList.add('--scroll');
+    } else {
+      header.classList.remove('--scroll');
+    }
   });
 }
 
@@ -144,6 +197,7 @@ function init() {
     center: center,
     zoom: 10
   });
+  myMap.behaviors.disable('scrollZoom');
   var marker = './img/marker.svg';
   var markerHover = './img/marker-hover.svg';
   var activeMarker;
@@ -235,6 +289,7 @@ var swiper = new swiper__WEBPACK_IMPORTED_MODULE_0__["default"]('.reports-slider
   \*********************************/
 /***/ (function() {
 
+var reportSlider = document.querySelector('.reports-slider ');
 var reportSlide = document.querySelectorAll('.report-slide');
 
 var fadeOut = function fadeOut(el, timeout) {
@@ -261,9 +316,11 @@ if (reportSlide) {
       reportSlide.forEach(function (elem, index) {
         var buttonPlay = elem.querySelector('.report__play');
         var video = elem.querySelector('video');
+        var trigger = elem.querySelector('.report-slide__trigger');
         video.pause();
         video.currentTime = 0;
         video.removeAttribute('controls');
+        trigger.classList.remove('--fade');
 
         if (ind != index) {
           fadeIn(buttonPlay, 300);
@@ -273,26 +330,40 @@ if (reportSlide) {
       });
     };
 
+    window.addEventListener('click', function (e) {
+      var target = e.target || e.srcElement || e.currentTarget;
+
+      if (!reportSlider.contains(target)) {
+        stopVideos(100);
+      }
+    });
     reportSlide.forEach(function (elem, index) {
       var buttonPlay = elem.querySelector('.report__play');
       var video = elem.querySelector('.report__video video');
-      buttonPlay.addEventListener('click', function () {
+      var trigger = elem.querySelector('.report-slide__trigger');
+      elem.addEventListener('click', function () {
         stopVideos(index);
-        video.play();
+        trigger.classList.add('--fade');
         fadeOut(buttonPlay, 300);
         video.setAttribute('controls', true);
         video.muted = false;
         elem.classList.add('--is-active');
+        setTimeout(function () {
+          video.currentTime = 0;
+          video.play();
+        }, 200);
       });
-      elem.addEventListener('mouseover', function () {
-        video.muted = true;
-        video.play();
-      });
-      elem.addEventListener('mouseout', function () {
-        video.muted = true;
-        video.pause();
-        video.currentTime = 0;
-      });
+
+      if (!elem.classList.contains('--is-active')) {
+        trigger.addEventListener('mouseover', function () {
+          video.muted = true;
+          video.play();
+        });
+        trigger.addEventListener('mouseout', function () {
+          video.pause();
+          video.currentTime = 0;
+        });
+      }
     });
   };
 }
